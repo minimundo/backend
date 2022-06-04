4b78bad8bab992b47b5e798cdf1d993e73dfba01
Kevin Almeida <janioklalmeida05@gmail.com>
2022-06-04 20:03:08 -0300

    :sparkles: feat: new migration added, teachers

---
+++ database/migrations/1654374596105_teachers.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Teachers extends BaseSchema {
+  protected tableName = 'teachers'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('cod_teacher')
+      table.string('name', 30).notNullable()
+      table.string('email', 50).unique().notNullable()
+      table.string('password', 20).notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
