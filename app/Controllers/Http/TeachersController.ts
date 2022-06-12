092727c231ed46eaafaae51a83bb49dc5cf4cdab
Kevin Almeida <janioklalmeida05@gmail.com>
2022-06-11 23:31:51 -0300

    :sparkles: feat: building method "register", to register new teachers from the route "teachers/new"

--- app/Controllers/Http/TeachersController.ts
+++ app/Controllers/Http/TeachersController.ts
@@ -1,3 +1,17 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class TeachersController {}
+import Teacher from 'App/Models/Teacher'
+
+export default class TeachersController {
+  public async register({ request, response }: HttpContextContract) {
+    const dataToCreate = request.body()
+
+    const teacher = await Teacher.create(dataToCreate)
+
+    response.status(201)
+
+    return {
+      message: 'successfully registered teacher',
+    }
+  }
+}
