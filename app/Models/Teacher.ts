538526479b22091ac0bc3a5051e093b841f93491
Kevin Almeida <janioklalmeida05@gmail.com>
2022-06-11 23:30:50 -0300

    :sparkles: feat: updating "Teacher" model based on "teachers" migration

--- app/Models/Teacher.ts
+++ app/Models/Teacher.ts
@@ -5,6 +5,15 @@ export default class Teacher extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  public name: string
+
+  @column()
+  public email: string
+
+  @column()
+  public password: string
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
