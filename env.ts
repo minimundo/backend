73f471c25e8e9dd24843a8b4ace34ab6cf0a607a
Kevin Almeida <janioklalmeida05@gmail.com>
2022-09-04 01:24:06 -0300

    🐛 returning changes from env.ts as it caused problems in deploy

--- env.ts
+++ env.ts
@@ -15,16 +15,11 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
+  DB_CONNECTION: Env.schema.string(),
   HOST: Env.schema.string({ format: 'host' }),
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
-  DB_CONNECTION: Env.schema.string(),
-  MYSQL_HOST: Env.schema.string(),
-  MYSQL_PORT: Env.schema.number(),
-  MYSQL_USER: Env.schema.string(),
-  MYSQL_PASSWORD: Env.schema.string.optional(),
-  MYSQL_DB_NAME: Env.schema.string(),
 })
