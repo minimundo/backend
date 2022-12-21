5e1c5fcf18f4e04069b749b5c9ac80e6b7675c33
Kevin Almeida <janioklalmeida05@gmail.com>
2022-12-21 02:51:27 -0300

    🧱 infra: new environment

--- env.ts
+++ env.ts
@@ -20,6 +20,7 @@ export default Env.rules({
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
+  APP_URL: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
   FIRST_EMAIL_SEED: Env.schema.string(),
